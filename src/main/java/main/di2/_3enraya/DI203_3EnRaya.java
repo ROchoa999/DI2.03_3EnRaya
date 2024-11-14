002bf30f4990bf4bca1add16a9a7afed975f74b9
Alumno <rodrigo.ochoa.018@gmail.com>
2024-11-14 17:28:24 +0100

    Second commit

---
+++ src/main/java/main/di2/_3enraya/DI203_3EnRaya.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.di2._3enraya;
+
+/**
+ *
+ * @author Alumno
+ */
+public class DI203_3EnRaya {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
